3f85d63fd1d19c215a5f7bf7d27ceb8a9032bc55
Floyd Zamarripa <floyd@floydasaur.us>
2017-07-31 15:57:55 -0500

    Creation of the script, it's blank, don't get your hopes up.

---
+++ embig.py
@@ -0,0 +1,8 @@
+##################################################
+#
+#                   Embiggenator.py
+#
+#
+##################################################
+
+# TODO:Write the whole script. Like, all of it. You've done nothing so far.
